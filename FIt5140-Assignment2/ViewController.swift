c72143b714d97fa360428adc5648050e4b44f137
Burns <burns@dyn-118-138-124-92.its.monash.edu.au>
2019-09-11 13:00:46 +1000

    Starter Version
    Contain google firebase

---
+++ FIt5140-Assignment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FIt5140-Assignment2
+//
+//  Created by Burns on 11/9/19.
+//  Copyright © 2019 Group 6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FIt5140-Assignment2Tests/FIt5140_Assignment2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  FIt5140_Assignment2Tests.swift
+//  FIt5140-Assignment2Tests
+//
+//  Created by Burns on 11/9/19.
+//  Copyright © 2019 Group 6. All rights reserved.
+//
+
+import XCTest
+@testable import FIt5140_Assignment2
+
+class FIt5140_Assignment2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
